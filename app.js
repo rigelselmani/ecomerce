5671bf563edf804b74e5cdfd0487ca1b37796676
rigel selmani <rigelrigelselmani@hotmail.com>
2020-07-06 13:56:44 -0400

    wodking on connecting atlas mongodb  and handling error on postman

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-const express= require("express");
+const express = require('express')
 const mongoose = require('mongoose');
-//import routes
-const userRoutes= require("./routes/user");
-// load env variables
-const dotenv = require('dotenv');
-dotenv.config()
+const morgan = require("morgan");
+const bodyParser=require("body-parser")
+const cookieParser=require("cookie-parser")
+require("dotenv").config();
+// app
+const app = express()
+// import routes
+const router=require("./routes/user");
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology: true 
+}).then(()=>console.log("DB Connected"));
 
-const app = express();
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// route middleware
+app.use('/api',router);
 
-const port = process.env.PORT ||8000;
-//db connection
-mongoose.connect(process.env.MONGO_URI,{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex:true
-})
-.then(() => console.log('DB Connected'))
- 
-mongoose.connection.on('error', err => {
-  console.log(`DB connection error: ${err.message}`)
-});
+const port=process.env.PORT ||3000
 
-app.use("/api",userRoutes);
-
-app.listen(port,()=>{
-    console.log("server is running on port ${port}");
-});
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- models/user.js
+++ models/user.js
@@ -1,66 +1,62 @@
-const mongoose = require("mongoose");
-const crypto = require("crypto");
-const uuidv1=require("uuid/v1");
-
+const mongoose=require("mongoose");
+const crypto=require("crypto");
+let uuidv1 = require('uuidv1');
 const userSchema=new mongoose.Schema({
-    name: {
+    name:{
         type:String,
         trim:true,
         required:true,
-        maxLength:32
+        maxlength:32
     },
-    email: {
-        type:String,
-        trim:true,
-        required:true,
-        unique:32
-    },
-    hashed_password: {
+    email:{
         type:String,
         trim:true,
         required:true,
         unique:32
     },
-    about: {
+    hashed_password:{
         type:String,
-        trim:true,
+        required:true
     },
-    salt: String,
-    role: {
+    about:{
+        type:String,
+        trim:true
+    },
+    salt:String,
+    role:{
         type:Number,
-        default:0
+        default:0,
     },
     history:{
         type:Array,
-        default:[]  
+        default: []
     }
-  }, 
-  { timestamps: true }
-);
+},{timestamps:true});
 
+//virtual field
 
-// virtual field
 userSchema.virtual("password")
 .set(function(password){
     this._password = password
-    this.salt= uuidv1()
+    this.salt=uuidv1();
     this.hashed_password=this.encryptPassword(password)
 })
-.get(function (){
-   return this._password
+.get(function(){
+    return this._password
 });
 
-userSchema.methods = {
+userSchema.methods={
     encryptPassword: function(password){
         if(!password) return"";
-        try {
-            return crypto.createHmac("sha1", this.salt)
-                            .update(password)
-                            .digest("hex")
+        try{
+            return crypto
+            .createHmac("sha1",this.salt)
+            .update(password)
+            .digest("hex");
         } catch (err){
-            return"";
+            return "";
         }
     }
 }
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
